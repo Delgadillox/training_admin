1e0930d4d58198eedee2283fda2df05d3f0f16ad
josepablodelgadillo <josepablodelgadillo@gmail.com>
2024-04-03 02:43:37 +0000

    Base fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-console.log('environment', process.env.VITE_ENV)
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_ENV === "production" ? '/admin/' : '/pruebas/admin',
-  plugins: [react()]
-})
+
+export default ({ mode }: { mode: string }) => {
+  const isProduction = mode === 'production';
+  const base = isProduction ? '/admin/' : '';
+
+  return defineConfig({
+    base: base,
+    plugins: [react()]
+  });
+};
