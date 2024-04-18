9e9380d2766784aedb3b4f8649b51a8b6d603d68
josepablodelgadillo <josepablodelgadillo@gmail.com>
2024-04-18 03:02:14 +0000

    Admin test

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default ({ mode }: { mode: string }) => {
-  const isProduction = mode === 'production';
-  const base = isProduction ? '/admin/' : '';
 
   return defineConfig({
-    base: base,
+    base: '/admin/',
     plugins: [react()]
   });
 };
