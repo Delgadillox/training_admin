59f2afc956bd1e8eb16a507fbad29ce56dc120e2
josepablodelgadillo <josepablodelgadillo@gmail.com>
2024-04-29 05:55:00 +0000

    router dom fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { router } from "./routes";
 
 const browserRouter = createBrowserRouter(router);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <RouterProvider router={browserRouter} />
